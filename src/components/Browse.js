6705670c0a7adc28f4b3b5488b565e802142bc32
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-30 19:25:46 -0800

    method to add API call

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,31 @@
+import { useEffect, useRef, useState } from "react"
 import { Link } from "react-router-dom"
 
 const Browse = () => {
+
+  const [corgiImage, setCorgiImage] = useState(null)
+
+  useEffect(() => {
+    console.log('useEffect')
+
+    const { REACT_APP_GIPHY_KEY } = process.env
+    console.log(REACT_APP_GIPHY_KEY)
+    
+    const getGIPHY = async () => {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${REACT_APP_GIPHY_KEY}&s=corgi`, { mode: 'cors' })
+      const corgiData = await response.json()
+      const src = corgiData.data.images.original.url
+      setCorgiImage(src)
+      // console.log(corgiData.data.images.original.url)
+    }
+
+    // getGIPHY()
+
+    return () => {
+      console.log('cleanup--when component unmounts')
+    }
+  }, [])
+
   return (
     <div>
       <h1>Browse</h1>
@@ -11,6 +36,7 @@ const Browse = () => {
           Call of Duty: Modern Warfare II
         </Link>
       </h1>
+      <img className="giphy" src={corgiImage} alt="Corgi"/>
     </div>
   )
 }
