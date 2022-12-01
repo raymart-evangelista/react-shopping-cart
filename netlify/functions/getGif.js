030020196f631abb14a20ee85cbb9901e01781e2
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-12-01 12:40:26 -0800

    fix

--- netlify/functions/getGif.js
+++ netlify/functions/getGif.js
@@ -1,23 +1,30 @@
 const axios = require("axios")
 
 exports.handler = async function(event, context) {
-  // console.log(event)
-  // console.log(context)
+  // const { search } = event.queryStringParameters
+  // return axios.get(`https://api.giphy.com/v1/gifs/translate?api_key=${process.env.GIPHY_SECRET}&s=${search}}`).then(response => {
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({ data: response.data.data})
+  //   }
+  // }).catch(error => {
+  //   console.log(error)
+  //   return {
+  //     statusCode: 404,
+  //     body: error.toString()
+  //   }
+  // })
 
   try {
     const { search } = event.queryStringParameters
-    console.log(search)
-    const response = await axios.get(`https://api.giphy.com/v1/gifs/translate?api_key=${process.env.GIPHY_SECRET}&s=${search}}`)
-    // console.log("*****")
-    // console.log(response)
-    // console.log("*****")
-    // console.log(JSON.stringify({ data: response}))
-    // console.log("*****")
+    // console.log(search)
+    const response = await axios.get(`https://api.giphy.com/v1/gifs/translate?api_key=${process.env.GIPHY_SECRET}&s=${search}`)
     return {
       statusCode: 200,
       body: JSON.stringify({ data: response.data.data }),
     }
   } catch (err) {
+    console.log(err)
     return {
       statusCode: 404,
       body: err.toString()
