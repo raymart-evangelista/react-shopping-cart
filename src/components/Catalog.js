82a623182ded46f11e3dcf018806eb05e0d95512
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-12-04 15:56:31 -0800

    fix fetch logic, creating item cards logic, add styling

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import ItemCard from "./ItemCard"
+import "./Catalog.css"
+import uniqid from "uniqid"
+// import fs from 'fs'
 
 const Catalog = () => {
 
   const [loading, setLoading] = useState(false)
   const [gameSrc, setGameSrc] = useState(null)
   const [gameInfo, setGameInfo] = useState(null)
+  const [games, setGames] = useState([])
+  const [itemCards, setItemCards] = useState(null)
 
   useEffect(() => {
-    console.log('[Catalog] useEffect')
-
     async function fetchGames() {
+      console.log(`fetching games...`)
       const API_ROOT = 'https://rawg.io/api'
       try {
+        console.log('trying')
         setLoading(true)
         const accessKey = process.env.REACT_APP_RAWG_SECRET
         // const endpoint = `${API_ROOT}/games?key=${accessKey}&dates=2019-09-01,2019-09-30&platforms=18,1,7`
@@ -22,9 +27,15 @@ const Catalog = () => {
         const endpoint = `${API_ROOT}/games?key=${accessKey}&platforms=187&page_size=40&ordering=-metacritic`
         const response = await fetch(endpoint)
         const searchedData = await response.json()
-        console.log(searchedData.results)
-        setGameSrc(searchedData.results[0].background_image)
-        setGameInfo(searchedData.results[0])
+        
+        // fs.writeFile('./api/games.txt', JSON.stringify(searchedData), function(err) {
+        //   if(err) {
+        //     return console.log(err)
+        //   }
+        //   console.log("The file was saved")
+        // })
+
+        setGames(searchedData.results)
       } catch (err) {
         console.log(`An error occurred: ${err}`)
       } finally {
@@ -32,18 +43,33 @@ const Catalog = () => {
       }
     }
 
-    fetchGames()
+    function createItemCards() {
+      console.log('creating item cards...')
+      const listItems = (games.map((game) =>
+        <ItemCard key={uniqid()} game={game} />
+      ))
+
+      setItemCards(listItems)
+    }
+
+    fetchGames().then(createItemCards())
 
     return () => {
       console.log('cleanup--when component unmounts')
     }
   }, [])
 
+
+
   return (
     <div>
-      <h1>This is catalog!</h1>
       {/* <p>{loading ? "Loading..." : <img src={gameSrc} alt="image"/>}</p> */}
-      {gameInfo && <ItemCard game={gameInfo}></ItemCard>}
+      {/* {!loading && listItems} */}
+      {/* {gameInfo && <ItemCard game={gameInfo}></ItemCard>} */}
+      <div className="item-container">
+        {!loading && itemCards}
+        {/* <p>{loading ? "Loading..." : {itemCards}}</p> */}
+      </div>
     </div>
   )
 }
--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,28 +2,14 @@ import { useEffect } from "react"
 import "./ItemCard.css"
 
 const ItemCard = ({game}) => {
-  useEffect(() => {
-    console.log('[ItemCard][useEffect]')
-    console.log(game)
-  })
 
   return (
-    <li>
-      {/* <div className="card">
-        <a className="" href="">
-          <img className="" style="width:100%" src={game.background_image} alt={game.slug} />
-          <div className="container">
-            <h1>{game.name}</h1>
-          </div>
-        </a>
-      </div> */}
-      <div className="card">
-        <img className="bg-img" src={game.background_image} alt={game.slug} />
-        <div className="container">
-          <h1>{game.name}</h1>
-        </div>
+    <div className="card">
+      <img className="bg-img" src={game.background_image} alt={game.slug} />
+      <div className="container">
+        <h1>{game.name}</h1>
       </div>
-    </li>
+    </div>
   )
 }
 
